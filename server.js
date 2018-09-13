8d18c1601e288c9db5aa828b40082c88e2fa9bc9
Peter King <73837king@gmail.com>
2018-09-13 06:28:59 -0400

    basic API fetch logic in place

---
+++ server.js
@@ -0,0 +1,71 @@
+'use strict';
+const express = require('express');
+const morgan = require('morgan');
+const request = require('request');
+
+// set in config.js
+const {
+  PORT,
+  CLIENT_ORIGIN,
+  API_ROOT_URL,
+  FS_CLIENT_ID,
+  FS_CLIENT_SECRET,
+  FOOD_CAT,
+  BAR_CAT
+} = require('./config');
+
+// locally set
+const RADIUS_2MI = `radius=${1609.34 * 2}`;
+// const RADIUS_1MI = `radius=${1609.34}`;
+// const RADIUS_05MI = `radius=${804.67}`;
+const LIMIT_50 = 'limit=50';
+
+const cors = require('cors');
+const corsOptions = {
+  origin: CLIENT_ORIGIN,
+};
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(cors(corsOptions));
+
+app.get('/', (req, res, next) => {
+  console.log('hello world');
+});
+
+app.get('/bites', (req, res, next) => {
+
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  const BITES_URL = `${API_ROOT_URL}?${FS_CLIENT_ID}&${FS_CLIENT_SECRET}&v=20180323&ll=${lat},${lng}&${RADIUS_2MI}&${LIMIT_50}&${FOOD_CAT}`;
+
+  // request to FourSquare API.
+  request(BITES_URL, (err, response, body) => {
+    if (err) {
+      return console.dir(err);
+    }
+    res.send(body);
+    next();
+  });
+});
+
+app.get('/bars', (req, res, next) => {
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+  const BARS_URL = `${API_ROOT_URL}?${FS_CLIENT_ID}&${FS_CLIENT_SECRET}&v=20180323&ll=${lat},${lng}&${RADIUS_2MI}&${LIMIT_50}&${BAR_CAT}`;
+
+  // request to FourSquare API.
+  request(BARS_URL, (err, response, body) => {
+    if (err) {
+      return console.dir(err);
+    }
+    res.send(body);
+    next();
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on port = ', PORT);
+});
